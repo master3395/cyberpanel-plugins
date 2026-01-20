9bcab288c364c2e913f6acc1f3437f01b9bca445
master3395 <master3395@users.noreply.github.com>
2026-01-20 01:50:23 +0100

    Update pm2Manager __init__.py

--- pm2Manager/__init__.py
+++ pm2Manager/__init__.py
@@ -5,5 +5,4 @@ Author: Master3395
 Version: 1.0.0
 """
 
-# Note: default_app_config is deprecated in Django 3.2+
-# The app is configured in apps.py with AppConfig
+default_app_config = 'pm2Manager.apps.Pm2ManagerConfig'
